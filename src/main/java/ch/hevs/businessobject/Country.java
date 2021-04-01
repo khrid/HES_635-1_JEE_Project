d2ef81a99d819bc5faacdaaf30c9105d2f3312e1
David <david.crittin@gmail.com>
2021-04-01 15:37:50 +0200

    Ajout Country code

--- src/main/java/ch/hevs/businessobject/Country.java
+++ src/main/java/ch/hevs/businessobject/Country.java
@@ -8,21 +8,29 @@ public class Country {
     // name
     @Column(name="country_name") // vu que c'est embedded, le nom risque d'être redondant avec une table appelante
     private String name;
+    // name
+    @Column(name="country_code") // vu que c'est embedded, le nom risque d'être redondant avec une table appelante
+    private String code;
 
     public Country() {
 
     }
-    public Country(String name) {
+    public Country(String name, String code) {
         this.name = name;
+        this.code = code;
     }
 
     public String getName() { return name; }
     public void setName(String name) { this.name = name; }
 
+    public String getCode() { return code; }
+    public void setCode(String code) { this.code = code; }
+
     @Override
     public String toString() {
         return "Country{" +
                 "name='" + name + '\'' +
+                "code='" + code + '\'' +
                 '}';
     }
 }
